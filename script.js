4753fb1000fd30c520d8c46de96294bb4fe30fc4
Nimira43 <n.rabone72@aol.com>
2024-11-24 23:28:10 +0000

    Ball rebounds off paddle - well kind of. Issues here.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Configuration constants 
 const PADDLE_WIDTH = 100; 
 const PADDLE_HEIGHT = 10; 
+const PADDLE_OFFSET = 60
 const BALL_RADIUS = 10; 
 const BALL_SPEED_X = 5; 
-const BALL_SPEED_Y = 5; 
+const BALL_SPEED_Y = 7; 
 const BACKGROUND_COLOUR = '#ff4500'; 
 const BALL_COLOUR = '#fff'; 
 const PADDLE_COLOUR = '#ffd700';
@@ -39,13 +40,32 @@ const updateAll = () => {
   drawAll()
 }
 
-// Moves the ball and handles boundary collisions
+// Ball reset positiion
+const ballReset = () => {
+  ballX = canvas.width / 2
+  ballY = canvas.height / 2
+}
+
+// Moves the ball, handles boundary collisions and resets the ball when it moves out of bounds at the bottom. 
 const moveAll = () => {
   ballX += ballSpeedX
   ballY += ballSpeedY
 
   if (ballX < 0 || ballX > canvas.width) ballSpeedX *= -1
-  if (ballY < 0 || ballY > canvas.height) ballSpeedY *= -1
+  if (ballY < 0) ballSpeedY *= -1
+  if (ballY > canvas.height) ballReset()
+
+  const paddleTop = canvas.height - PADDLE_OFFSET 
+  const paddleBottom = paddleTop + PADDLE_HEIGHT 
+  const paddleLeft = paddleX 
+  const paddleRight = paddleLeft + PADDLE_WIDTH 
+
+  if (ballY > paddleTop &&
+      ballY < paddleBottom &&
+      ballX > paddleLeft &&
+      ballY < paddleRight) {
+        ballSpeedY *= -1
+      }
 }
 
 // Draws all game elements on the canvas
