313895d1a6c6a7dbb965cd1fa2076f927a83197e
Nimira43 <n.rabone72@aol.com>
2024-11-24 21:51:36 +0000

    Ball is animating and rebounding off walls

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 let canvas
 let ctx
 let ballX = 75
+let ballY = 75
+let ballSpeedX = 5
+let ballSpeedY = 5
 
 window.onload = () => {
   canvas = document.getElementById('game-canvas')
   ctx = canvas.getContext('2d')
   const fps = 30
-  setInterval(updateAll, 1000 / fps)
-  
+  setInterval(updateAll, 1000 / fps) 
 }
 
 function updateAll() {
-  ballX += 5
+  ballX += ballSpeedX
+  ballY += ballSpeedY
+
+  if (ballX < 0) ballSpeedX *= -1
+  if (ballX > canvas.width) ballSpeedX *= -1
+  if (ballY < 0) ballSpeedY *= -1
+  if (ballY > canvas.height) ballSpeedY *= -1
+
+
   ctx.fillStyle = '#ff4500'
   ctx.fillRect(0, 0, canvas.width, canvas.height)
   ctx.fillStyle = 'white'
   ctx.beginPath()
-  ctx.arc(100, 100, 10, 0, Math.PI * 2, true)
+  ctx.arc(ballX, ballY, 10, 0, Math.PI * 2, true)
   ctx.fill()
 }
