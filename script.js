86b8ac9bcae91e81e60c65576a3be4a26a42ef56
Nimira43 <n.rabone72@aol.com>
2024-11-24 22:52:09 +0000

    Paddle movement working okay. Time for a big refactor next.

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+const paddleWidth = 100
+const paddleHeight = 10
+let paddleX = 400
+
 let canvas
 let ctx
+
 let ballX = 75
 let ballY = 75
 let ballSpeedX = 5
 let ballSpeedY = 5
+
+function updateMousePos(e) {
+  let rect = canvas.getBoundingClientRect()
+  let mouseX = e.clientX - rect.left - document.documentElement.scrollLeft
+  paddleX = mouseX - paddleWidth / 2
+}
 
 window.onload = () => {
   canvas = document.getElementById('game-canvas')
   ctx = canvas.getContext('2d')
   const fps = 30
   setInterval(updateAll, 1000 / fps) 
+  canvas.addEventListener('mousemove', updateMousePos)
 }
 
 const updateAll = () => {
@@ -21,15 +33,14 @@ const moveAll = () => {
   ballX += ballSpeedX
   ballY += ballSpeedY
 
-  if (ballX < 0) ballSpeedX *= -1
-  if (ballX > canvas.width) ballSpeedX *= -1
-  if (ballY < 0) ballSpeedY *= -1
-  if (ballY > canvas.height) ballSpeedY *= -1
+  if (ballX < 0 || ballX > canvas.width) ballSpeedX *= -1
+  if (ballY < 0 || ballY > canvas.height) ballSpeedY *= -1
 }
 
 const drawAll = () =>  {
   colourRect(0, 0, canvas.width, canvas.height, '#ff4500')
   colourCircle(ballX, ballY, 10, '#fff')
+  colourRect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight, '#111')
 }
 
 const colourRect = (topLeftX, topLeftY, boxWidth, boxHeight, fillColour) => {
