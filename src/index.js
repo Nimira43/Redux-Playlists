355034cbd09fbb8193850ba88c4d816fbfca9e12
Nimira43 <n.rabone72@aol.com>
2025-01-17 09:30:54 +0000

    Upgraded some React packages and brought in React Router DOM. Using React 18.3.1 now. Project up and running. Will concentrate a bit on the CSS next.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import 'bulma/css/bulma.css'
-import { createRoot } from 'react-dom/client'
-import App from '..App'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-const rootElement = document.getElementById('root')
-const root = createRoot(rootElement)
-
-root.render(
-  <App />
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 )
