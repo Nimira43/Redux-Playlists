e7360bb546914a1cb1105880eb069214ce82a6d3
Nimira43 <n.rabone72@aol.com>
2025-01-17 08:33:26 +0000

    Created code for data/index.js - creating fake movie and song names

---
+++ src/data/index.js
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker/locale/en'
+
+export const createRandomMovie = () => {
+  return `
+    ${faker.word.adjective()} ${faker.word.noun()}
+  `
+}
+
+export const createRandomSong = () => {
+  return faker.music.songName()
+}
